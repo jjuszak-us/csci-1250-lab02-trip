18643540f7966de34f36df8a9eb192a15e73ec54
Joseph <joejuszak@gmail.com>
2026-09-23 10:38:32 -0400

    Part 1

---
+++ TripCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+*Name: Joseph Juszak
+*Course: CSCI 1250 Section 001
+*Assignment: Lab 2, Trip Calculator
+*Date: September 23, 2026
+*Description: A calculator for a trip with friends
+*/
+
+
+Console.WriteLine("How many miles is the destination? ");
+double destinationMileage = Convert.ToDouble(Console.ReadLine ());
+
+Console.WriteLine("How many miles per gallon does your car use? ");
+double milesPerGallon = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("What is the price of gas per gallon? ");
+double priceOfGasPerGallon = Convert.ToDouble(Console.ReadLine());
+
+double gallonsNeeded = (destinationMileage * 2) / milesPerGallon;
+
+double FuelCost = gallonsNeeded * priceOfGasPerGallon;
+
+Console.WriteLine("Gallons needed:" + gallonsNeeded.ToString("F2"));
+Console.WriteLine("Fuel Cost:" + FuelCost.ToString("C"));
+
+
+
+
+
+
+
+
+
+
+
+
